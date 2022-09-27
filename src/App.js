0fbe6e342e914b359c21d9a7ea2a3f25fe4ec772
pcbennett108 <pcbennett42@gmail.com>
2022-09-27 14:07:49 -0600

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  );
-};
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You've changed the counter to " + counter);
+  }, [counter]);
+
   return (
     <div className="App">
-      <Person name="John" lastName="Doe" age={27} />
-      <Person name="Mary" lastName="Doe" age={29} />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
 };
