9cee46e9684d5f6fcdf44f5a66996870c354f26c
pcbennett108 <pcbennett42@gmail.com>
2022-09-28 13:29:26 -0600

    move movie card to seperate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 
+import MovieCard from "./MovieCard";
+
 import "./App.css";
 import SearchIcon from "./search.svg";
 
@@ -39,25 +41,7 @@ const App = () => {
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-            <img
-              src={
-                movie1.Poster !== "N/A"
-                  ? movie1.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie1.Title}
-            />
-          </div>
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
-        </div>
+        <MovieCard movie1={movie1} />
       </div>
     </div>
   );
