3193c8707d0fbbc81a701dcf6b062ebbe8ad261f
pcbennett108 <pcbennett42@gmail.com>
2022-09-28 12:38:52 -0600

    Add search and styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from "react";
 import { useEffect } from "react";
 
 import "./App.css";
-import searchIcon from "./search.svg";
+import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=beb5ce4d";
+
+const movie1 = {
+  Title: "Superman, Spiderman or Batman",
+  Year: "2011",
+  imdbID: "tt2084949",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
+};
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -18,7 +27,27 @@ const App = () => {
     searchMovies("spiderman");
   }, []);
 
-  return <h1></h1>;
+  return (
+    <div className="app">
+      <h1>MovieNite</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Spiderman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
