d45d86ada2e1f962aa1217f848616a7040a4b0eb
pcbennett108 <pcbennett42@gmail.com>
2022-09-28 13:58:55 -0600

    connect cards to API and map through array, add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import MovieCard from "./MovieCard";
 
@@ -17,15 +17,17 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
-    searchMovies("spiderman");
+    searchMovies("superman");
   }, []);
 
   return (
@@ -35,14 +37,23 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Spiderman"
+          value="Superman"
           onChange={() => {}}
         />
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
